cc3c818503cacfcb6e7f770d4a53fb9982582d97
Stas <tymniaks@gmail.com>
2021-10-18 21:14:05 +0300

    Added DAL Tests

---
+++ DALTests/AuctionRepositoryTest.cs
@@ -0,0 +1,75 @@
+﻿using DAL.EntityFramework;
+using DAL.Interfaces;
+using Domain;
+using Microsoft.EntityFrameworkCore.Storage;
+using NUnit.Framework;
+using System;
+
+using System.Linq;
+
+namespace DALTests
+{
+    [TestFixture]
+
+    public class AuctionRepositoryTest
+    {
+        private string _connection = @"Server=(localdb)\MSSQLLocalDB;Database=tradingCompanyTest;Trusted_Connection=True;";
+
+        IRepository<Auction> auctionRep;
+        [OneTimeSetUp]
+        public void InitialSetupTest()
+        {
+            auctionRep = new AuctionRepository(_connection);
+        }
+
+        [Test]
+        public void GetByIdTest()
+        {
+            var res = auctionRep.Get(7);
+            Assert.IsTrue(res.AuctionName == "Iphone 12 Auction");
+        }
+
+        [Test]
+        public void CreateTest()
+        {
+            Auction a = new Auction
+            {
+                AuctionName = "Ready Player One Auction",
+                StrtupPrice = 10,
+                RedemptionPrice = 50,
+                ProductId = 5
+            };
+            var res = auctionRep.Create(a);
+            Assert.IsTrue(res.AuctionName == "Ready Player One Auction");
+        }
+
+        [Test]
+        public void GetAllTest()
+        {
+            var auction = auctionRep.GetAll();
+
+            Assert.IsTrue(auction.Any(a => a.AuctionName == "Iphone 12 Auction"));
+
+        }
+        [Test]
+        public void UpdateTest()
+        {
+            Auction a = new Auction
+            {
+                AuctionName = "Iphone 12 Auction",
+                StrtupPrice = 10,
+                RedemptionPrice = 50,
+                ProductId = 5
+            };
+            auctionRep.Update(7, a);
+            Assert.IsTrue(auctionRep.Get(7).AuctionName == "Iphone 12 Auction");
+        }
+
+        [Test]
+        public void DeleteTest()
+        {
+            auctionRep.Delete(14);
+            Assert.IsTrue(!auctionRep.GetAll().Any(a => a.AuctionId == 14));
+        }
+    }
+}
---
+++ DALTests/ProductRepositoryTest.cs
@@ -0,0 +1,75 @@
+﻿using DAL.EntityFramework;
+using DAL.Interfaces;
+using Domain;
+using Microsoft.EntityFrameworkCore.Storage;
+using NUnit.Framework;
+using System;
+
+using System.Linq;
+
+namespace DALTests
+{
+    [TestFixture]
+
+    public class ProductRepositoryTest
+    {
+        private string _connection = @"Server=(localdb)\MSSQLLocalDB;Database=tradingCompanyTest;Trusted_Connection=True;";
+
+        IRepository<Product> productRep;
+        [OneTimeSetUp]
+        public void InitialSetupTest()
+        {
+            productRep = new ProductRepository(_connection);
+        }
+
+        [Test]
+        public void GetByIdTest()
+        {
+            var res = productRep.Get(1);
+            Assert.IsTrue(res.ProductName == "Iphone 12");
+        }
+
+        [Test]
+        public void CreateTest()
+        {
+            Product p = new Product
+            {
+                ProductName="Duna",
+                CategoryId=10,
+                RowInsertTime = DateTime.Now,
+                RowUpdateTime = DateTime.Now
+            };
+            var res = productRep.Create(p);
+            Assert.IsTrue(res.ProductName == "Duna");
+        }
+
+        [Test]
+        public void GetAllTest()
+        {
+            var categories = productRep.GetAll();
+
+            Assert.IsTrue(categories.Any(c => c.ProductName == "Arius 5 Pro"));
+
+        }
+        [Test]
+        public void UpdateTest()
+        {
+            Product p = new Product
+            {
+                ProductName = "Iphone 12",
+                CategoryId = 5,
+                RowInsertTime = DateTime.Now,
+                RowUpdateTime = DateTime.Now
+            };
+            productRep.Update(1, p);
+            Assert.IsTrue(productRep.Get(1).ProductName == "Iphone 12");
+        }
+
+        [Test]
+        public void DeleteTest()
+        {
+            productRep.Delete(3);
+            Assert.IsTrue(!productRep.GetAll().Any(c => c.CategoryId == 3));
+        }
+    }
+}
